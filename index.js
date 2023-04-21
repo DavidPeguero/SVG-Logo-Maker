06a1373593d209ac91b39d6910c3a4e34388f7c7
David Peguero <david.peguero.cancel@gmail.com>
2023-04-20 22:04:07 -0400

    Created questions to prompt when the program runs

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 //Import inquirer 
 const inquirer = require("inquirer");
 
+//Questions to ask through inquirer
+const questions = [
+    {
+        name : 'test', 
+        type : 'input',
+        message:'Enter desired text (up to three characters)',
+    },
+    {
+        name : 'test', 
+        type : 'input',
+        message:'Enter desired color (name or hexadecimal)',
+    },
+    {
+        name : 'test', 
+        type : 'input',
+        message:'Enter desired shape (circle, shape, or triangle)',
+    }
+]
