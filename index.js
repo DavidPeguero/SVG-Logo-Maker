a511ba4148d86dcf6a9d15d4ffcfed0865abe0b0
David Peguero <david.peguero.cancel@gmail.com>
2023-04-20 22:12:47 -0400

    Created a function that allows you prompt for the text in the svg

--- index.js
+++ index.js
@@ -1,21 +1,39 @@
 //Import inquirer 
 const inquirer = require("inquirer");
 
+//Set variables need
+let text;
+let input;
+let shape;
+
 //Questions to ask through inquirer
 const questions = [
     {
-        name : 'test', 
-        type : 'input',
-        message:'Enter desired text (up to three characters)',
-    },
-    {
-        name : 'test', 
+        name : 'color', 
         type : 'input',
         message:'Enter desired color (name or hexadecimal)',
     },
     {
-        name : 'test', 
+        name : 'shape', 
         type : 'input',
         message:'Enter desired shape (circle, shape, or triangle)',
     }
 ]
+
+//Begin the prompts
+
+
+function getText(){
+    inquirer.
+    prompt({
+        name : 'text', 
+        type : 'input',
+        message:'Enter desired text (up to three characters)',
+    }).
+    then((data) => {
+        text = data.text;
+        console.log(text)
+    });
+}
+
+getText();
