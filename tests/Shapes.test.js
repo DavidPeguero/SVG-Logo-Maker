5e2e6cd87ba8e565d526784890bf98bfa81ee15c
David Peguero <david.peguero.cancel@gmail.com>
2023-04-22 17:16:14 -0400

    Made test for square shapre

--- tests/Shapes.test.js
+++ tests/Shapes.test.js
@@ -1,18 +1,14 @@
 //Imports
-const Shapes = require('../lib/shapes');
+const { Triangle, Square, Circle } = require('../lib/shapes');
+const { describe, expect, toEqual } = require('jest')
 
 //Shape testing suite
-describe('Shapes', () => {
-    describe('Square', () => {
-        
-     })
-    
-    describe('Triangle', () => {
-
-     })
-    
-    describe('Circle', () => { 
-
-     })
-
+describe('Square', () => {
+    describe('Color Set', () => {
+        it('should set the color equal to blue', () => {
+            const shape = new Square();
+            shape.setColor('blue');
+            expect(shape.render()).toEqual(`<rect width="300" height="200" fill='blue'></rect>`)
+        }
+    });
 })
