3b24d2667ee463d1605c2616938b2242e6e5634c
David Peguero <david.peguero.cancel@gmail.com>
2023-04-22 17:05:49 -0400

    Set up testing structure

--- tests/Shapes.test.js
+++ tests/Shapes.test.js
@@ -1,13 +1,18 @@
 //Imports
 const Shapes = require('../lib/shapes');
 
-describe('SVG', () => { 
-    describe('Character Length', () => {
-        it('should fail if the length is greater than three', ()=> {
-            const text = 'TEST';
-            const Shape = () => new Shape(text)
-            const err = new Error('Input is greater than three characters');
-            expect(svg).toThrowError(err);
-        });
-    });
- });
+//Shape testing suite
+describe('Shapes', () => {
+    describe('Square', () => {
+        
+     })
+    
+    describe('Triangle', () => {
+
+     })
+    
+    describe('Circle', () => { 
+
+     })
+
+})
