d0a93f033d78e03cf29aafa1490be09eaf876df7
David Peguero <david.peguero.cancel@gmail.com>
2023-04-22 17:38:03 -0400

    Created basic color setting tests for all shapes

--- tests/Shapes.test.js
+++ tests/Shapes.test.js
@@ -10,5 +10,26 @@ describe('Square', () =>{
             expect(shape.render()).toEqual(`<rect width='300' height='200' fill='blue'/>`)
         })
     })
-
 })
+
+describe('Triangle', () =>{
+    describe('Set Color', () => {
+        it('should pass if it fills the shape with blue', () => {
+            const shape = new Triangle();
+            shape.setColor('blue');
+            expect(shape.render()).toEqual(`<polygon points='150, 18 244, 182 56, 182' fill='blue' />`)
+        })
+    })
+})
+
+
+describe('Circle', () =>{
+    describe('Set Color', () => {
+        it('should pass if it fills the shape with blue', () => {
+            const shape = new Circle();
+            shape.setColor('blue');
+            expect(shape.render()).toEqual(`<circle cx='150' cy='100' r='80' fill='blue'/>`)
+        })
+    })
+})
+
