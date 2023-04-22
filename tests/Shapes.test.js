f60d52635d2c47a7e2c4a2d727cea5f4fdbf42c4
David Peguero <david.peguero.cancel@gmail.com>
2023-04-22 17:34:04 -0400

    Added a successful test for Square shape

--- tests/Shapes.test.js
+++ tests/Shapes.test.js
@@ -1,14 +1,14 @@
 //Imports
 const { Triangle, Square, Circle } = require('../lib/shapes');
-const { describe, expect, toEqual } = require('jest')
 
 //Shape testing suite
-describe('Square', () => {
-    describe('Color Set', () => {
-        it('should set the color equal to blue', () => {
+describe('Square', () =>{
+    describe('Set Color', () => {
+        it('should pass if it fills the shape with blue', () => {
             const shape = new Square();
             shape.setColor('blue');
-            expect(shape.render()).toEqual(`<rect width="300" height="200" fill='blue'></rect>`)
-        }
-    });
+            expect(shape.render()).toEqual(`<rect width='300' height='200' fill='blue'/>`)
+        })
+    })
+
 })
