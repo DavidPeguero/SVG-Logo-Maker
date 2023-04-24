2363685fa8f5fd221a806afce1340e9868cc75f5
David Peguero <david.peguero.cancel@gmail.com>
2023-04-24 19:39:46 -0400

    Adde tests for the svg generator.js

---
+++ tests/svgGenerator.test.js
@@ -0,0 +1,27 @@
+//Import svgGenerator
+const svgGenerator = require("../lib/svgGenerator");
+const { Triangle } = require('../lib/shapes');
+const Text = require('../lib/text')
+
+describe('SVG Generator', () => {
+    describe('Render SVG', () => {
+        it('should pass if the expected svg code for a triangle with a blue color and "svg" text is returned', () => {
+            const text = 'svg';
+            const shape = 'triangle'
+            const svgBase = new Triangle();
+            const color = 'blue';
+            svgBase.setColor(color);
+            const svgText = new Text(shape);
+            svgText.setText(text);
+
+            const svgGen = new svgGenerator(svgBase, svgText);
+            expect(svgGen.render()).toEqual(`<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+
+        <polygon points='150, 18 244, 182 56, 182' fill='blue' />
+      
+        <text x="150" y="150" font-size="60" text-anchor="middle" fill="white">svg</text>
+
+      </svg>`)
+        });
+    });
+});
